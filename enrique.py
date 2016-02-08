b16bfd81399c6d6accd6ac1d70ecf5019635a873
hfaran <hamzafaran@outlook.com>
2016-02-07 17:15:01 -0800

    Momentarily adding TSPSA specifics

--- enrique.py
+++ enrique.py
@@ -2,7 +2,7 @@
 
 import sys
 import threading
-import time
+from time import time
 import math
 import random
 import json
@@ -11,7 +11,33 @@ import mesos.interface
 from mesos.interface import mesos_pb2
 import mesos.native
 
-#import magic_client
+# XXX Hardcode client for testing
+sys.path.append("/home/vagrant/traveling_sailor")
+import traveling_sailor
+# latitude and longitude for the twenty largest U.S. cities
+cities = {
+    'New York City': (40.72, 74.00),
+    'Los Angeles': (34.05, 118.25),
+    'Chicago': (41.88, 87.63),
+    'Houston': (29.77, 95.38),
+    'Phoenix': (33.45, 112.07),
+    'Philadelphia': (39.95, 75.17),
+    'San Antonio': (29.53, 98.47),
+    'Dallas': (32.78, 96.80),
+    'San Diego': (32.78, 117.15),
+    'San Jose': (37.30, 121.87),
+    'Detroit': (42.33, 83.05),
+    'San Francisco': (37.78, 122.42),
+    'Jacksonville': (30.32, 81.70),
+    'Indianapolis': (39.78, 86.15),
+    'Austin': (30.27, 97.77),
+    'Columbus': (39.98, 82.98),
+    'Fort Worth': (32.75, 97.33),
+    'Charlotte': (35.23, 80.85),
+    'Memphis': (35.12, 89.97),
+    'Baltimore': (39.28, 76.62)
+}
+
 
 class MyExecutor(mesos.interface.Executor):
     def registered(self, driver, executorInfo, frameworkInfo, slaveInfo):
@@ -45,8 +71,11 @@ class MyExecutor(mesos.interface.Executor):
             print "start: cooling rate is: ".format(cooling_rate)
             print "start: num_mutations is: ".format(num_mutations)
 
-            e = math.e
+            # XXX
+            problem = traveling_sailor.TSPSA()
+            problem.init(cities)
 
+            e = math.e
             best_fitness = parent_fitness = None
             best_key = location
             T = temperature
@@ -57,8 +86,8 @@ class MyExecutor(mesos.interface.Executor):
                 print("Key: {}".format(parent_key))
                 start = time()
                 for i in xrange(num_mutations):
-                    new_key = magic_mutation(parent_key) #from client library
-                    fitness = magic_fitness_score(new_key) #from client library
+                    new_key = problem.mutation(parent_key)
+                    fitness = problem.fitness_score(new_key)
                     if parent_fitness is None:
                         parent_fitness = fitness
                         parent_key = new_key
