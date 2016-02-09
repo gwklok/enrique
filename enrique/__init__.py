771f9f0df7dc1d1fa4722e6becca2a5133b3ec59
hfaran <hamzafaran@outlook.com>
2016-02-08 16:16:41 -0800

    Dynamic import of task; resolves #5

--- enrique/__init__.py
+++ enrique/__init__.py
@@ -1,45 +1,15 @@
 #!/usr/bin/python
 import sys
 import threading
-from time import time
-import math
-import random
 import json
+import imp
 
 import mesos.interface
 from mesos.interface import mesos_pb2
-#import mesos.native
-
-# XXX Hardcode client for testing
-sys.path.append("/home/vagrant/traveling-sailor")
-sys.path.append("../traveling-sailor")
-import traveling_sailor
-# latitude and longitude for the twenty largest U.S. cities
-cities = {
-    'New York City': (40.72, 74.00),
-    'Los Angeles': (34.05, 118.25),
-    'Chicago': (41.88, 87.63),
-    'Houston': (29.77, 95.38),
-    'Phoenix': (33.45, 112.07),
-    'Philadelphia': (39.95, 75.17),
-    'San Antonio': (29.53, 98.47),
-    'Dallas': (32.78, 96.80),
-    'San Diego': (32.78, 117.15),
-    'San Jose': (37.30, 121.87),
-    'Detroit': (42.33, 83.05),
-    'San Francisco': (37.78, 122.42),
-    'Jacksonville': (30.32, 81.70),
-    'Indianapolis': (39.78, 86.15),
-    'Austin': (30.27, 97.77),
-    'Columbus': (39.98, 82.98),
-    'Fort Worth': (32.75, 97.33),
-    'Charlotte': (35.23, 80.85),
-    'Memphis': (35.12, 89.97),
-    'Baltimore': (39.28, 76.62)
-}
+import mesos.native
 
 
-class MyExecutor(mesos.interface.Executor):
+class Enrique(mesos.interface.Executor):
     def registered(self, driver, executorInfo, frameworkInfo, slaveInfo):
         print "Executor registered"
 
@@ -61,37 +31,33 @@ class MyExecutor(mesos.interface.Executor):
             parsed_json = json.loads(task.data)
             uid = parsed_json['uid'] #just echo
             location = parsed_json['location'] #starting search location
-            # temperature = parsed_json['temperature'] #starting temperature, decreases
-            # cooling_rate = parsed_json['cooling_rate'] #final
-            # num_mutations = parsed_json['num_mutations'] #final
+            job_data = parsed_json['job_data']
             task_seconds = parsed_json['task_seconds']
             task_name = parsed_json['task_name']
 
             print "start: uid is: ".format(uid)
             print "start: location is: ".format(location)
-            # print "start: temperature is: ".format(temperature)
-            # print "start: cooling rate is: ".format(cooling_rate)
-            # print "start: num_mutations is: ".format(num_mutations)
 
             # XXX
-            tsp = traveling_sailor.TSPSA(cities, location)
-            tsp.copy_strategy = "slice"
-            auto_schedule = tsp.auto(task_seconds/60.0)
-            tsp.set_schedule(auto_schedule)
-            best_key, best_fitness = tsp.anneal()
+            pkg = imp.load_source(task_name, '/home/vagrant')
+            annealer = pkg.Problem(job_data, location)
+            auto_schedule = annealer.auto(minutes=task_seconds/60.0)
+            annealer.set_schedule(auto_schedule)
+            best_key, best_fitness = annealer.anneal()
 
             print "Best Fitness: {}".format(best_fitness)
             print "Best Key: {}".format(best_key)
             print "end: uid is: ".format(uid)
             print "end: location is: ".format(location)
-            # print "end: temperature is: ".format(temperature)
-            # print "end: cooling rate is: ".format(cooling_rate)
-            # print "end: num_mutations is: ".format(num_mutations)
 
             update = mesos_pb2.TaskStatus()
             update.task_id.value = task.task_id.value
             update.state = mesos_pb2.TASK_FINISHED
-            update.data = json.dumps({"uid": uid},{"best_location": best_key},{"fitness_score": best_fitness})
+            update.data = json.dumps(
+                {"uid": uid},
+                {"best_location": best_key},
+                {"fitness_score": best_fitness}
+            )
             driver.sendStatusUpdate(update)
 
         thread = threading.Thread(target=run_task)
@@ -104,5 +70,5 @@ class MyExecutor(mesos.interface.Executor):
 
 def main():
     print "Starting executor"
-    driver = mesos.native.MesosExecutorDriver(MyExecutor())
+    driver = mesos.native.MesosExecutorDriver(Enrique())
     sys.exit(0 if driver.run() == mesos_pb2.DRIVER_STOPPED else 1)
